e3a3a4e873226fecbe1983b3a36eedde83167119
Deeka0 <62205832+Deeka0@users.noreply.github.com>
2023-09-20 17:07:43 +0100

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,515 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from sys import platform
+import time, os, subprocess, pathlib, glob
+
+
+
+options = Options()
+options.add_argument("--headless")
+options.page_load_strategy = 'eager'
+service = Service(executable_path="/Users/dark/Downloads/geckodriver")
+runtime_path = str(pathlib.Path(__file__).parent.resolve())
+
+
+def clean_up():
+    os.chdir(path=runtime_path)
+    files = glob.glob("*")
+    for file in files:
+        if file.endswith(".log"):
+            os.remove(file)
+    try:
+        files = glob.glob("/Users/dark/Documents/Dev/*")
+        for file in files:
+            if file.endswith(".log"):
+                os.remove(file)
+    except:
+        pass
+
+
+class RSSID:
+    def __init__(self) -> None:
+        pass
+
+    def rssid_mac():
+        rssid_value = subprocess.run(["/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport", "-I"], 
+                                    stdout=subprocess.PIPE, 
+                                    text=True)
+
+        rssid_value = rssid_value.stdout.split()
+        rssid = rssid_value[27]
+        return rssid
+
+    def rssid_windows():
+        subprocess_result = subprocess.Popen('netsh wlan show interfaces',
+        shell=True,
+        stdout=subprocess.PIPE)
+        subprocess_output = subprocess_result.communicate()[0],subprocess_result.returncode
+        
+        try:
+            rssid_value = subprocess_output[0].decode('utf-8')
+            rssid_value = rssid_value.split()
+
+            if (rssid_value[30] == "BSSID"):
+                rssid = rssid_value[29]
+                return rssid
+            elif (rssid_value[31] == "BSSID"):
+                rssid = " ".join([rssid_value[29], rssid_value[30]])
+                return rssid
+            elif (rssid_value[32] == "BSSID"):
+                rssid = " ".join([rssid_value[29], rssid_value[30], rssid_value[31]])
+                return rssid
+            
+        except UnicodeDecodeError:
+            rssid_value = str(subprocess_output).split()
+        
+            if (rssid_value[73] == "\\r\\n\\r\\n"):
+                rssid = rssid_value[72]
+                return rssid
+            elif (rssid_value[74] == "\\r\\n\\r\\n"):
+                rssid = " ".join([rssid_value[72], rssid_value[73]])
+                return rssid
+            elif (rssid_value[75] == "\\r\\n\\r\\n"):
+                rssid = " ".join([rssid_value[72], rssid_value[73], rssid_value[74]])
+                return rssid
+    
+    def rssid_linux():
+        pass
+
+
+def display(rssid_init, isp=None, network_mode=None, switch=None, connection=None, state=None):
+    print(f"SSID: {rssid_init}")
+    print(f"ISP: {isp}".upper())
+    print(f"Band: {network_mode}".upper())
+    print(f"Internet: {switch}".upper())
+    print(f"Connection: {connection}".upper())
+    print(f"State: {state}".upper())
+
+
+def clear(command):
+    return os.system(command)
+
+
+def session_firefox():
+    clean_up()
+    clear(command=clear_arg)
+    print("Spawning session...")
+    global driver, wait
+    driver = webdriver.Firefox(options=options, service=service)
+    wait = WebDriverWait(driver, timeout=30)
+    driver.get("http://192.168.0.1/")
+    return time.sleep(5)
+
+
+def session_safari():
+    desired_cap = {
+        "browserName": "safari",
+        "browserVersion": "14.3", # iOS version
+        "platformName": "iOS",
+        "safari:deviceType": "iPhone",
+        "safari:deviceName": "Dark's iPhone", # device name
+        "safari:deviceUDID": "2995549f51d2b632e9bb46c211151a15527ea969" # UDID from previous step
+        }
+    clean_up()
+    clear(command=clear_arg)
+    print("Spawning session...")
+    global driver, wait
+    driver = webdriver.Safari(desired_capabilities=desired_cap)
+    wait = WebDriverWait(driver, timeout=30)
+    driver.get("http://192.168.0.1/")
+    return time.sleep(5)
+
+
+try:
+
+    if platform == "darwin":
+        clear_arg = "clear"
+        desktop_location = r"/Users/dark/Desktop/balance.png"
+        rssid_init = RSSID.rssid_mac()
+    elif platform == "win32":
+        clear_arg = "cls"
+        desktop_location = r"C:\Users\Dark\Desktop\balance.png"
+        rssid_init = RSSID.rssid_windows()
+    elif (platform == "linux") or (platform == "linux2"):
+        clear_arg = "clear"
+        desktop_location = r"/home/dark/Desktop/balance.png"
+        rssid_init = RSSID.rssid_linux()
+    else:
+        print("OS not available yet")
+
+    if platform == "ios":
+        clear_arg = "clear"
+        desktop_location = r"/Users/dark/Desktop/balance.png"
+        rssid_init = None
+        session_safari()
+    else:
+        session_firefox()
+    clear(command=clear_arg)
+except:
+    driver.quit()
+    exit("Router unavailable")
+
+
+class Auth:
+    def __init__(self) -> None:
+        pass
+
+    def login():
+        settings_button = driver.find_element(By.ID, "menu2")
+        wait.until(EC.element_to_be_clickable(settings_button)).click()
+        time.sleep(2)
+
+        username_button = driver.find_element(By.ID, "tbarouter_username")
+        password_button = driver.find_element(By.ID, "tbarouter_password")
+        signin_btn = driver.find_element(By.ID, "btnSignIn")
+
+        username_button.send_keys(f"{username}")
+        time.sleep(1)
+        print("Logging in")
+        password_button.send_keys(f"{password}")
+        time.sleep(1)
+        wait.until(EC.element_to_be_clickable(signin_btn)).click()
+        time.sleep(3)
+        
+        # Indicate if network is locked
+        try:
+            cancel = driver.find_element(By.CSS_SELECTOR, "#confirmDlg > a:nth-child(2)")
+            if (driver.find_element(By.CSS_SELECTOR, "#lt_confirmDlg_title").is_displayed) and (cancel.is_displayed):
+                cancel.click()
+                print("SIM RESTRICTED")
+                time.sleep(1)
+        except:
+            pass
+        print("Logged in successfully")
+        return time.sleep(2)
+
+    def logout():
+        print("Logging out")
+        time.sleep(1)
+        logout_btn = driver.find_element(By.ID, "MainLogOut")
+        logout_btn.click()
+        time.sleep(3)
+        print("Logged out successfully")
+        return time.sleep(2)
+
+
+class Balance:
+    def __init__(self) -> None:
+        pass
+
+    def balance_check_glo():
+        balance_checker_button = driver.find_element(By.CSS_SELECTOR, "#mBalanceChecker > a")
+        balance_checker_button.click()
+        time.sleep(3)
+        ussd_field = driver.find_element(By.CSS_SELECTOR, "#txt_send_ussd")
+        ussd_send = driver.find_element(By.CSS_SELECTOR, "#btn_send_ussd_code")
+        ussd_field.send_keys("*323#")
+        time.sleep(1)
+        ussd_send.click()
+        print("Checking balance")
+        time.sleep(10)
+
+        driver.save_screenshot(desktop_location)
+        print("Screenshot of balance inquiry has been saved to desktop")
+        time.sleep(2)
+
+
+    def balance_check_airtel():
+        balance_checker_button = driver.find_element(By.CSS_SELECTOR, "#mBalanceChecker > a")
+        balance_checker_button.click()
+        time.sleep(3)
+        ussd_field = driver.find_element(By.CSS_SELECTOR, "#txt_send_ussd")
+        ussd_send = driver.find_element(By.CSS_SELECTOR, "#btn_send_ussd_code")
+        ussd_field.send_keys("*323#")
+        time.sleep(1)
+        ussd_send.click()
+        print("Checking balance")
+        time.sleep(10)
+
+        # View in messages
+
+        driver.save_screenshot(desktop_location)
+        print("Screenshot of balance inquiry has been saved to desktop")
+        time.sleep(2)
+        
+    
+    def balance_check_mtn():
+        pass
+
+    def balance_check_9mobile():
+        pass
+
+
+def isp_choice():
+    char = ('1','2','3','4','0','x','X')
+    clear(command=clear_arg)
+    print("\t\t\t\tSelect an ISP or network provider\t\t\t\t\n")
+    print("1. 9mobile")
+    print("2. Glo")
+    print("3. Airtel")
+    print("4. MTN")
+    print("")
+    print("0. Main menu")
+    print("X. Exit")
+    
+    asker = input("\nEnter here: ").strip()
+    if asker not in char:
+        print("Invalid option")
+        time.sleep(1)
+        return isp_choice()
+    elif (asker == 'x') or (asker == 'X'):
+        return True
+    elif asker == "0":
+        return False
+    
+    try:
+        if (asker == "1") or (asker == "4"):
+            print("Unavailable")
+            time.sleep(1)
+            return isp_choice()
+        elif asker == "2":
+            Balance.balance_check_glo()
+        elif asker == "3":
+            Balance.balance_check_airtel()
+    finally:
+        return isp_choice()
+
+
+def band_switch():
+    char = ('1','2','3','4','5','6','0','x','X')
+    clear(command=clear_arg)
+    print("\t\t\t\tChoose an option\t\t\t\t\n")
+    
+    print("1. 4G throttle")
+    print("2. 3G throttle")
+
+    print("3. 4G only")
+    print("4. 4G/3G")
+    print("5. 3G only")
+    print("6. 2G only")
+
+    print("")
+    print("0. Main menu")
+    print("X. Exit")
+
+    asker = input("\nEnter here: ").strip()
+    if asker not in char:
+        print("\t\t\tInvalid option!")
+        time.sleep(1)
+        return band_switch()
+    elif (asker == 'x') or (asker == 'X'):
+        return True
+    elif asker == "0":
+        return False
+    
+    try:
+        mode_select = driver.find_element(By.CSS_SELECTOR, "#network_selectModeType")
+        fourGO = driver.find_element(By.CSS_SELECTOR, "#type_4g_only")
+        fourG_threeG = driver.find_element(By.CSS_SELECTOR, "#type_4g3g")
+        threeGO = driver.find_element(By.CSS_SELECTOR, "#type_3g_only")
+        twoGO = driver.find_element(By.CSS_SELECTOR, "#dropdown2Gonly")
+        save_btn = driver.find_element(By.CSS_SELECTOR, "#btn_network_mode_apply")
+        
+        if (asker == "1"):
+            print("Throttling 4G...")
+            threeGO.click()
+            save_btn.click()
+            time.sleep(10)
+            wait.until(EC.element_to_be_clickable(mode_select)).click()
+            fourGO.click()
+            
+        elif asker == "2":
+            print("Throttling 3G...")
+            fourGO.click()
+            save_btn.click()
+            time.sleep(10)
+            wait.until(EC.element_to_be_clickable(mode_select)).click()
+            threeGO.click()
+
+        if (asker == "3"):
+            fourGO.click()
+        elif (asker == "4"):
+            fourG_threeG.click()
+        elif asker == "5":
+            threeGO.click()
+        elif asker == "6":
+            twoGO.click()
+
+    except:
+        pass
+    finally:
+        print("Saving configurations")
+        time.sleep(1)
+        save_btn.click()
+        time.sleep(10)
+        wait.until(EC.element_to_be_clickable(mode_select)).click()
+        print("Done")
+        time.sleep(2)
+        return band_switch()
+    
+
+def decider(connection_state):
+
+    char = ('1','2','3','4','5','6','r','R','x','X')
+    arg = None
+    clear(command=clear_arg)
+    try:
+        logout_btn_check = driver.find_element(By.ID, "MainLogOut").is_displayed()
+        switch_status = driver.find_element(By.CSS_SELECTOR, "#txtConnected").get_property("innerText")
+        curr_network_mode = driver.find_element(By.CSS_SELECTOR, "#txtSystemNetworkMode").get_property("innerText")
+        curr_isp = driver.find_element(By.CSS_SELECTOR, "#txtNetworkOperator").get_property("innerText")
+
+        if logout_btn_check:
+            curr_state = "Logged In"
+        else:
+            curr_state = "Logged Out"
+
+        if switch_status == "Disconnected":
+            curr_switch = "Off"
+        else:
+            curr_switch = "On"
+
+    except:
+        pass
+    display(rssid_init, isp=curr_isp, network_mode=curr_network_mode, switch=curr_switch, connection=connection_state, state=curr_state)
+
+    print("\n\t\t\t\tChoose an option\t\t\t\t\n")
+    print("1. Login")
+    print("2. Logout")
+    print("3. Test internet")
+    print("4. Internet switch")
+    print("5. Balance check")
+    print("6. Throttle network / Band switch")
+    print("")
+    print("R. Refresh")
+    print("X. Exit")
+
+    asker = input("\nEnter here: ").strip()
+    if asker not in char:
+        print("\t\t\tInvalid option!")
+        time.sleep(1)
+        return decider(connection_state=arg1)
+    elif (asker == 'r') or (asker == 'R'):
+        return decider(connection_state=connection_state)
+    elif (asker == 'x') or (asker == 'X'):
+        driver.quit()
+        return exit()
+   
+    try:
+        logout_btn_check = driver.find_element(By.ID, "MainLogOut").is_displayed()
+        switch_btn = driver.find_element(By.CSS_SELECTOR, "#switchBtn_connStatus > div")
+        
+        if asker == '1':
+            if logout_btn_check:
+                print("Already logged in")
+            else:
+                Auth.login()
+            time.sleep(1)
+
+        elif asker == '2':
+            if logout_btn_check:
+                Auth.logout()
+            else:
+                print("Already logged out")
+            time.sleep(1)
+
+        elif asker == '3':
+            original_window = driver.current_window_handle
+            driver.switch_to.new_window('tab')
+            try:
+                driver.get("https://www.google.com/")
+                time.sleep(3)
+                arg1 = "active"
+                print("Connection active")
+            except:
+                arg1 = "inactive"
+                print("Connection inactive, try throttling")
+            finally:
+                driver.close()
+                driver.switch_to.window(original_window)
+                time.sleep(1)
+        
+        elif asker == '4':
+            wait.until(EC.element_to_be_clickable(switch_btn)).click()
+            print("Switching...")
+            time.sleep(8)
+            print("Done")
+            time.sleep(8)
+       
+        elif asker == '5':
+            if not logout_btn_check:
+                Auth.login()
+            print("Loading configurations...")
+            arg = isp_choice()
+
+        elif asker == '6':
+            if not logout_btn_check:
+                Auth.login()
+
+            print("Loading configurations...")
+            settings_button = driver.find_element(By.ID, "menu2")
+            wait.until(EC.element_to_be_clickable(settings_button)).click()
+            time.sleep(2)
+
+            connection = driver.find_element(By.CSS_SELECTOR, "#mInternetConn > a")
+            wait.until(EC.element_to_be_clickable(connection)).click()
+            time.sleep(2)
+            
+            mode_select = driver.find_element(By.CSS_SELECTOR, "#network_selectModeType")
+            mode_select.click()
+
+            arg = band_switch()
+    except:
+        print("Exception occured")
+    finally:
+        if arg:
+            driver.quit()
+            return exit()
+        
+        wait.until(EC.element_to_be_clickable(driver.find_element(By.ID, "menu1"))).click()
+        return decider(connection_state=arg1)
+
+
+
+
+if __name__ == "__main__":
+
+    if rssid_init == "WireNet_A":
+        username = "admin"
+        password = "QqqqqqqQ"
+    elif rssid_init == "Supreme":
+        username = "admin"
+        password = "admin"
+    else:
+        print("WiFi network is unregistered for automation")
+    
+    try:
+        arg1 = None
+        decider(connection_state=arg1)
+    except:
+        driver.quit()
+        if exit:
+            exit("Exiting")
+        else:
+            exit("Critical error")
+
+
+
+# '''
+# ip route
+# [default via 172.16.9.2 dev eth0 proto dhcp metric 100
+# 172.16.9.0/24 dev eth0 proto kernel scope link src 172.16.9.130 metric 100]
+
+# 4
+# 11
+# '''
+
+
+"""
+path to user
+
+"""
